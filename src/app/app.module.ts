da5083f2c621b8e653b686e4a713a6b2e1450f54
HansonWzh <wzh_hanson@126.com>
2018-08-29 20:41:55 +0800

    2018-08-29

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { MessagesComponent } from './messages/messages.component';
+import { HeroDetailComponent } from './poets/hero-detail.component';
+import { HeroesComponent } from './poems/heroes.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -20,8 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     DashboardComponent,
     HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent
+    HeroDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
